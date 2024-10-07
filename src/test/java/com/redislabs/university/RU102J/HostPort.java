71b07c1ee1ab1676ec662bcd45b023571c8bb30c
matth <mamatthe@students.cs.ubc.ca>
2024-10-06 23:27:25 -0700

    first commit

--- src/test/java/com/redislabs/university/RU102J/HostPort.java
+++ src/test/java/com/redislabs/university/RU102J/HostPort.java
@@ -3,7 +3,7 @@ package com.redislabs.university.RU102J;
 public class HostPort {
     final private static String defaultHost = "localhost";
     final private static Integer defaultPort = 6379;
-    final private static String defaultPassword = "";
+    final private static String defaultPassword = "foobared";
 
     public static String getRedisHost() {
         return defaultHost;
